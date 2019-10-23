4b35c5f87ae83170fe86883c47e638cd1d02a596
ViktorJava <gipsyscrew@yandex.ru>
2019-10-23 10:26:53 +0300

    Добавил два класса Array

---
+++ src/arrays/ArrayCopyTest.java
@@ -0,0 +1,61 @@
+package arrays;
+
+/**
+ * Хорошо известно, что нативная функция Java System.arraycopy () является полезным способом копирования одного
+ * массива в другой, поскольку она является нативной.
+ * Чтобы ответить на эти вопросы, я сделал простой тест и запустил его как программу Java на своем ПК
+ * <p>
+ * Вот результаты при запуске его с моего компьютера (Java 11, 8 ГБ памяти, процессор Intel i5):
+ * <p>
+ * Размер массива: 10_000_000
+ * Наивный алгоритм - 139 мс
+ * System.arraycopy(); - 20 мс
+ *
+ * источник: http://www.aviyehuda.com/blog/2011/06/25/android-quick-tip-use-system-arraycopy/
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 23.10.2019
+ */
+public class ArrayCopyTest {
+
+    private static final int SIZE_OF_ARRAY = 10_000_000;
+    private static long time;
+
+
+    public static void main(String[] args) {
+
+        Integer[] sourceArray = new Integer[SIZE_OF_ARRAY];
+        Integer[] destinationArray = new Integer[SIZE_OF_ARRAY];
+        fillArray(sourceArray);
+
+        startBenchmark();
+        naiveCopy(sourceArray, destinationArray);
+        stopBenchmark();
+
+        startBenchmark();
+        System.arraycopy(sourceArray, 0, destinationArray, 0, sourceArray.length);
+        stopBenchmark();
+    }
+
+    private static void naiveCopy(Integer[] src, Integer[] dst) {
+        for (int i = 0; i < src.length; i++) {
+            dst[i] = src[i];
+        }
+    }
+
+    private static void fillArray(Integer[] src) {
+        for (int i = 0; i < src.length; i++) {
+            src[i] = i;
+        }
+    }
+
+    private static void startBenchmark() {
+        time = System.currentTimeMillis();
+    }
+
+    private static void stopBenchmark() {
+        time = System.currentTimeMillis() - time;
+        System.out.println("time=" + time);
+    }
+
+}
---
+++ src/arrays/ArrayTest.java
@@ -0,0 +1,42 @@
+package arrays;
+
+/**
+ * простые операции с массивом
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 23.10.2019
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+        System.out.print("присвоение значения массиву arr[4]=10: ");
+        int[] arr = new int[10];
+        arr[4] = 10;
+        System.out.println(arr[4]);
+
+        System.out.print("заполнение массива циклом do while:");
+        int i = 0;
+        do {
+            arr[i] = i;
+            System.out.print(arr[i] + " ");
+            i++;
+        } while (i < arr.length);
+
+        System.out.println();
+
+        System.out.print("заполнение массива циклом for: ");
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = j;
+            System.out.print(arr[j] + " ");
+        }
+
+        System.out.println();
+
+        System.out.print("Вывод массива foreach: ");
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+        //объявление и инициализация массива
+        int[] small = {1, 2, 3};
+    }
+}
