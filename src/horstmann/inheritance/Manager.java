7a176120c7f833644bf40788904d4d4b5acc423d
ViktorJava <gipsyscrew@yandex.ru>
2019-12-05 08:58:37 +0200

    Наследование Хорстманна
    1. реализация супер класса Employee
    2. реализация подкласса Manager
    3. реализация main класса для тестирования классов

---
+++ src/horstmann/inheritance/Manager.java
@@ -0,0 +1,35 @@
+package horstmann.inheritance;
+
+/**
+ * Класс Manager расширяет класс Employee.
+ * перегружается родительский класс getSalary
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 05.12.2019
+ */
+public class Manager extends Employee {
+    private double bonus;
+
+    /**
+     * @param n     имя сотрудника.
+     * @param s     зарплата.
+     * @param year  год приёма на работу.
+     * @param month месяц приёма на работу.
+     * @param day   день приёма на работу.
+     */
+    Manager(String n, double s, int year, int month, int day) {
+        super(n, s, year, month, day);
+        bonus = 0;
+    }
+
+    @Override
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    void setBonus(double b) {
+        bonus = b;
+    }
+}
