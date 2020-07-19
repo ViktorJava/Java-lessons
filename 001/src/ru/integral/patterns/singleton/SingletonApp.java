33cb007a5388ac9cd04bc7d21a0fd666b051ece3
ViktorJava <gipsyscrew@yandex.ru>
2020-07-19 14:42:10 +0300

    Реализация потокобезопасного шаблона проектирования Singleton.

---
+++ 001/src/ru/integral/patterns/singleton/SingletonApp.java
@@ -0,0 +1,20 @@
+package ru.integral.patterns.singleton;
+
+/**
+ * Проверяем синглтон на предмет его работы.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 13.01.2020
+ */
+public class SingletonApp {
+
+    public static void main(String[] args) {
+        Singleton[] array = new Singleton[100];
+        for (int i = 0; i < 100; i++) {
+            array[i] = Singleton.getInstance();
+        }
+        System.out.println(Singleton.counter);
+    }
+}
+
